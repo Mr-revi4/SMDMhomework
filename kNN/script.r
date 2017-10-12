e8fbd8b443e90121860249a14a9502b91b3cd6ea
Mr-revi4-Home <mister.revi4@yandex.ru>
2017-10-12 17:34:46 +0300

    implementation of kNN

--- kNN/script.r
+++ kNN/script.r
@@ -1,5 +1,26 @@
-install.packages("ggplot2")
-library(ggplot2)
-ggplot(data=iris, aes(x = Petal.Length, y = Petal.Width)) +
-  geom_point(aes(color=Species)) +
-  xlab("Petal Length") +  ylab("Petal Width")
+#library(ggplot2)
+#ggplot(data=iris, aes(x = Petal.Length, y = Petal.Width)) +
+  #geom_point(aes(color=Species)) +
+  #xlab("Petal Length") +  ylab("Petal Width")
+euclideanDistance <- function(u, v)
+{
+  sqrt(sum((u - v)^2))
+}
+sortObjectsByDist <- function(xl, z, metricFunction = euclideanDistance)
+{
+  l <- dim(xl)[1]
+  n <- dim(xl)[2] - 1
+  distances <- matrix(NA, l, 2)
+  for (i in 1:l)
+  {
+    distances[i, ] <- c(i, metricFunction(xl[i, 1:n], z))
+  }
+  orderedXl <- xl[order(distances[, 2]), ]
+  return (orderedXl);
+}
+
+kNN <- function()
+{
+  orderedXl <- sortObjectsByDist(xl = iris[,3:5], z = c(1,2))
+}
+
